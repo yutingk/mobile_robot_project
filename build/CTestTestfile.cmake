a36a3cd7bf48b302ce3b728f18cce3e7bba51ce8
yutingk <98682859+yutingk@users.noreply.github.com>
2022-12-21 21:07:07 +0800

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/catkin_ws/src
-# Build directory: /home/robot/catkin_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("cp3")
--- build/cp3/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/robot/catkin_ws/src/cp3
-# Build directory: /home/robot/catkin_ws/build/cp3
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
